fffd6bee38e1511358c8d38ba6f2326c574c8a57
Biswajit Dey <biswajitdey.one@gmail.com>
2023-10-02 09:51:24 +0530

    Add  admin Panel & update authentication

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser =  require('body-parser');
+// for admin authentication
 const session = require('express-session');
 
 const port = 3000;
@@ -10,22 +11,25 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-// Set up express-session middleware
+// Set up express-session middleware (we use this for admin panel authentication)
 app.use(session({
     secret: 'c957fa32c75dcd49f0becb7346e566b529a4795ef001a640b6b0ea1c74dea2ca', // Replace with a strong secret key
     resave: true,
     saveUninitialized: true
 }));
 
+// Home route
 
 app.get("/",(req,res)=>{
     res.render("index.ejs");
 })
 
+//Login route
 app.get("/Admin_login",(req,res)=>{
     res.render("partials/loginPage.ejs");
 })
 
+//master Id & key for login
 const masterId = "master12494290@admin.secure.self";
 const masterKey = "tRg!v&z#2$v2$9p&z#L6"
 
@@ -40,16 +44,19 @@ const authenticateUser = (req, res, next) => {
     }
 };
 
+//If our backend verify the id & key then admin will access the admin panel route
 app.get("/adminPage", authenticateUser,(req,res)=>{
 
     res.render("partials/adminPanel.ejs");
 })
 
+//error 404 page
 app.get("/errorPage",(req,res)=>{
 
     res.render("partials/random404.ejs");
 })
 
+//this is the post method with help of this we get the user typing password and then we verify the user
 app.post("/submit",async(req,res)=>{
 
     const adminId = req.body.adminId;
@@ -76,7 +83,9 @@ app.post("/submit",async(req,res)=>{
     }
 })
 
-
+app.get("/addNews", authenticateUser,(req,res)=>{
+    res.render("partials/addNews.ejs");
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
